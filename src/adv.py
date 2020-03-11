b38bfafda9764fa46a0831ee80ecbd99a0807aa5
Mohammad Tourjoman <mtourjoman01@hotmail.com>
2020-03-11 14:44:00 -0700

    Scattered a couple of items

--- src/adv.py
+++ src/adv.py
@@ -8,14 +8,14 @@ import os
 
 room = {
     'outside':  Room("Outside Cave Entrance",
-                     "North of you, the cave mount beckons", [Item("Crate", 'I am box')]),
+                     "North of you, the cave mount beckons", [Item("Flashlight", "How convenient")]),
 
     'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
 passages run north and east."""),
 
     'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
 into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm."""),
+the distance, but there is no way across the chasm.""", [Item('Key', 'Looks ancient. It has a skull engraving on it')]),
 
     'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
 to north. The smell of gold permeates the air."""),
